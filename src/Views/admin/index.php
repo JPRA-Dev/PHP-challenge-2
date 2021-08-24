f5e702870a672418db73adf0cd5a07c5b51d7390
Emerson Vandeputte <emersonvandeputte@gmail.com>
2021-08-24 16:30:54 +0200

    table

--- src/Views/admin/index.php
+++ src/Views/admin/index.php
@@ -1,5 +1,185 @@
 <?php
 ?>
+
+
 <main>
-    <h1>Admin home page</h1>
-</main>
+    <h1>Hello, Jean-Christian! <br>
+What do you want to do today?</h1>
+
+<div class="button">
+<input class="favorite styled"
+       type="button"
+       value="New_invoices">
+
+</div>
+
+<div class="button">
+<input class="favorite styled"
+       type="button"
+       value="New_compagnies">
+       
+</div>
+
+<div class="button">
+<input class="favorite styled"
+       type="button"
+       value="New_contact">
+       
+</div>
+
+
+
+<div class="container">
+<table>
+   <caption class="table_last_invoices">Last Invoices </caption>
+   <div class="colum">
+   <tr>
+       <th>Invoice Number</th>
+       <th>Dates</th>
+       <th>compagny</th>
+       <th></th>
+   </tr>
+</div>
+<div class="table_body">
+   <tr>
+       <td>F20190404-004</td>
+       <td>04/04/2019</td>
+       <td>Jouets Jean-Michel</td>
+       <td>🗑️</td>
+   </tr>
+   <tr>
+       <td>F20190404-004</td>
+       <td>04/04/2019</td>
+       <td>Dundler Mifflin</td>
+       <td>🗑️</td>
+   </tr>
+   <tr>
+       <td>F20190404-003</td>
+       <td>04/04/2019</td>
+       <td>Pierre Cailloux</td>
+       <td>🗑️</td>
+   </tr>
+   <tr>
+       <td>F20190404-001</td>
+       <td>04/04/2019</td>
+       <td>Pied Pipper</td>
+       <td>🗑️</td>
+   </tr>
+   <tr>
+       <td>F20190403-654</td>
+       <td>03/04/2019</td>
+       <td>Raviga</td>
+       <td>🗑️</td>
+   </tr>
+   </div>
+
+</table>
+</div>
+
+<div class="container">
+<table>
+   <caption class="table_last_compagnies">Last Compagnies</caption>
+   <div class="colum">
+   <tr>
+       <th>Names</th>
+       <th>TVA</th>
+       <th>country</th>
+       <th>Compagnies</th>
+       <th></th>//delete
+   </tr>
+</div>
+<div class="table_body">
+   <tr>
+       <td>Raviga</td>
+       <td>US456 654 342</td>
+       <td>United States</td>
+       <td>Fournisseur</td>
+       <td>🗑️</td>
+   </tr>
+   <tr>
+       <td>Dunder Mifflin</td>
+       <td>US678 765 765</td>
+       <td>United States</td>
+       <td>Clients</td>
+       <td>🗑️</td>
+   </tr>
+   <tr>
+       <td>Pierre Cailloux</td>
+       <td>FR 678 908 654</td>
+       <td>France</td>
+       <td>Fournisseur</td>
+       <td>🗑️</td>
+   </tr>
+   <tr>
+       <td>Belgalol</td>
+       <td>BE0876 654 665</td>
+       <td>Belgique</td>
+       <td>Fournisseur</td>
+       <td>🗑️</td>
+   </tr>
+   <tr>
+       <td>Jouets Jean-Michel</td>
+       <td>FR 677 544 000</td>
+       <td>France</td>
+       <td>Clients</td>
+       <td>🗑️</td>
+   </tr>
+   </div>
+
+</table>
+</div>
+
+<div class="container">
+<table>
+   <caption class="table_last_contact">Last Compagnies</caption>
+   <div class="colum">
+   <tr>
+       <th>Names</th>
+       <th>phone</th>
+       <th>E-mail</th>
+       <th>compagny</th>
+       <th></th>
+   </tr>
+</div>
+<div class="table_body">
+   <tr>
+       <td>Peter Gregory</td>
+       <td>555-4567</td>
+       <td>peter.gregory@raviga.com</td>
+       <td>Raviga</td>
+       <td>🗑️</td>
+   </tr>
+   <tr>
+       <td>Cameron Howe</td>
+       <td>555-78967</td>
+       <td>cam.howe@mutiny.net</td>
+       <td>Mutiny</td>
+       <td>🗑️</td>
+   </tr>
+   <tr>
+       <td>Gavin Belson</td>
+       <td>555-4213</td>
+       <td>gavin@hooli.com</td>
+       <td>Hoolir</td>
+       <td>🗑️</td>
+   </tr>
+   <tr>
+       <td>Jiang Yang</td>
+       <td>555-4567</td>
+       <td>jian.yan@phoque.off</td>
+       <td>Phoque Off</td>
+       <td>🗑️</td>
+   </tr>
+   <tr>
+       <td>Bertram Gilfoyle</td>
+       <td>555-098</td>
+       <td>gilfoyle@piedpiper.com</td>
+       <td>Pied Piper</td>
+       <td>🗑️</td>
+   </tr>
+   </div>
+
+</table>
+</div>
+</mains>
+
