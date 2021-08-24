184c26eaa82a245ba2ea009f386774c8154dd5f1
juju2307 <metzjulie2@gmail.com>
2021-08-24 16:57:51 +0200

    debut css

--- src/Views/admin/addcontact.php
+++ src/Views/admin/addcontact.php
@@ -1,5 +1,47 @@
 <?php
+
 ?>
 <main>
     <h1>Add contact form</h1>
-</main>
+  <div class="container">
+	  <div class="container1">
+	<form action="" method="post">
+		<div class="lastname">
+			<label for="name">Last Name :</label>
+			<br>
+			<input class="input" type="text" name="name" value="" placeholder="Name..." autocomplete="off" required>
+		</div>	
+
+		<div class="firstname">
+			<label for="firstname">First Name :</label>
+			<br>
+			<input class="input" type="text" name="firstname" value="" placeholder="Firstname..." autocomplete="off" required>
+		</div>
+
+		<div class="phone">
+			<label for="phone">Phone :</label>
+			<br>
+			<input class="input" type="tel" name="phone" placeholder="Phone..." required>
+		</div>
+
+		<div class="email">
+		<label for="pwd">Email:</label>
+		<br>
+		<input class="input" type="email" name="email" placeholder="Email@..." required><br>
+		</div>
+
+		<div class="company">
+		<label for="company">Company :</label>
+		<br>
+		<input class="input" type="text" name="company" placeholder="Company..." required><br>
+		</div>
+		<br>
+		<div class="button">
+		<input  type="hidden" name="token" value="<">
+		<button class="submit" type="submit" name="submit">Submit</button>
+		</div>
+	</form>
+    </main>
+   </div>
+  </div>
+
