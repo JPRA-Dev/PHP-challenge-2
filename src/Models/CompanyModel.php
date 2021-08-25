dbb1e41eb517597a93060bdef1cc3d2eb36c7a39
Elsa Magalhaes <elsa.magalhaes@gmail.com>
2021-08-25 16:29:24 +0200

    models_update

--- src/Models/CompanyModel.php
+++ src/Models/CompanyModel.php
@@ -3,31 +3,68 @@
 
 namespace App\Models;
 
+use App\Helpers\ConfigHelper;
+use Exception;
 
 class CompanyModel extends Model
 {
-    public function find($params)
-    {
+    public $_data;
 
+
+    public function find($params=null)
+    {
+        if($params){
+            $field= (is_numeric($params)) ? 'id' : 'name';
+            $data = $this->getDB()->get('company', array($field, '=', $params));
+
+            if($data->count()){
+                $this->_data = $data;
+                return true;
+
+            }
+        }
+        return false;
     }
 
-    public function findOne($params)
+    public function findOne($params=null)
     {
+        if($params){
+            $field= (is_numeric($params)) ? 'id' : 'name';
+            $data = $this->getDB()->get('company', array($field, '=', $params));
 
+            if($data->count()){
+                $this->_data = $data->first();
+                return true;
+
+            }
+        }
+        return false;
     }
 
     public function update($fields = array(),$id=null)
     {
+        if(!$id){
+            $id=$this->data()->id;
+        }
 
+        if(!$this->getDB()->update('company',$id,$fields)){
+            throw new Exception('There was a problem updating.');
+        }
     }
 
     public function create($fields = array())
     {
-
+        if(!$this->getDB()->insert('company',$fields)){
+            throw new Exception('There was a problem creating a company.');
+        }
     }
 
     public function delete($id)
     {
-
+        $this->getDB()->delete('company', ['id', '=',$id]);
+    }
+    
+    public function data(){
+        return $this->_data;
     }
 }
--- src/Models/InvoiceModel.php
+++ src/Models/InvoiceModel.php
@@ -3,8 +3,68 @@
 
 namespace App\Models;
 
+use Exception;
+
 
 class InvoiceModel extends Model
 {
+    public $_data;
 
+
+    public function find($params=null)
+    {
+        if($params){
+            $field= (is_numeric($params)) ? 'id' : 'nbrinvoice';
+            $data = $this->getDB()->get('invoices', array($field, '=', $params));
+
+            if($data->count()){
+                $this->_data = $data;
+                return true;
+
+            }
+        }
+        return false;
+    }
+
+    public function findOne($params=null)
+    {
+        if($params){
+            $field= (is_numeric($params)) ? 'id' : 'nbrinvoice';
+            $data = $this->getDB()->get('invoices', array($field, '=', $params));
+
+            if($data->count()){
+                $this->_data = $data->first();
+                return true;
+
+            }
+        }
+        return false;
+    }
+
+    public function update($fields = array(),$id=null)
+    {
+        if(!$id){
+            $id=$this->data()->id;
+        }
+
+        if(!$this->getDB()->update('invoices',$id,$fields)){
+            throw new Exception('There was a problem updating.');
+        }
+    }
+
+    public function create($fields = array())
+    {
+        if(!$this->getDB()->insert('invoices',$fields)){
+            throw new Exception('There was a problem creating an invoice.');
+        }
+    }
+
+    public function delete($id)
+    {
+        $this->getDB()->delete('invoices', ['id', '=',$id]);
+    }
+    
+    public function data(){
+        return $this->_data;
+    }
 }
