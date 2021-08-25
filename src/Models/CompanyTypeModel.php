0f189af9da83e06c608276cb88c5fb756218cce4
Elsa Magalhaes <elsa.magalhaes@gmail.com>
2021-08-25 16:37:54 +0200

    company_type model updte

--- src/Models/CompanyTypeModel.php
+++ src/Models/CompanyTypeModel.php
@@ -6,5 +6,36 @@ namespace App\Models;
 
 class CompanyTypeModel extends Model
 {
-    
+    public $_data;
+
+
+    public function find($params=null)
+    {
+        if($params){
+            $field= (is_numeric($params)) ? 'id' : 'type';
+            $data = $this->getDB()->get('company_type', array($field, '=', $params));
+
+            if($data->count()){
+                $this->_data = $data;
+                return true;
+
+            }
+        }
+        return false;
+    }
+
+    public function findOne($params=null)
+    {
+        if($params){
+            $field= (is_numeric($params)) ? 'id' : 'type';
+            $data = $this->getDB()->get('company_type', array($field, '=', $params));
+
+            if($data->count()){
+                $this->_data = $data->first();
+                return true;
+
+            }
+        }
+        return false;
+    }
 }
