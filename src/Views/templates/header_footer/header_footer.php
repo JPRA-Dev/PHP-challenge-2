d262078289af7fa3ec62cad54e6b8ccd108a6b31
Joao Andrade <jpandrade.dev@gmail.com>
2021-08-26 12:24:06 +0100

    fix meta links header/footer

--- src/Views/templates/header_footer/header_footer.php
+++ src/Views/templates/header_footer/header_footer.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="utf-8">
-<title>Implement Sticky Header and Footer with CSS</title>
-<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" media="all">
-<link rel=stylesheet href="assets/css/style.css">
-<link rel=stylesheet href="assets/css/reset.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <link rel=stylesheet href="assets/css/style.css">
+    <link rel=stylesheet href="assets/css/reset.css">
 </head>
 <body>
     <header>
