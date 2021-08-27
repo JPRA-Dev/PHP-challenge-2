a945d9fad6f86b263bdc0bf1d111528f66614c6c
juju2307 <metzjulie2@gmail.com>
2021-08-27 11:48:41 +0200

    indexvieuwcss

--- src/Views/index.php
+++ src/Views/index.php
@@ -1,3 +1,166 @@
+<?php
+?>
+
 <main>
-    <h1>Home Page</h1>
+    <h1>Welcome to the COGIP</h1>
+
+        <div class="container">
+
+            <table class="container1">
+
+                <theader>
+                    <tr>
+                        <th>Last Invoices</th>
+                    </tr>
+                    <tr>
+                        <th>Invoice Number</th>
+                        <th>Dates</th>
+                        <th>compagny</th>
+                        <th></th>
+                   </tr>
+                </theader>
+
+                <tbody>
+                   <tr class="row1">
+                       <td>F20190404-004</td>
+                       <td>04/04/2019</td>
+                       <td>Jouets Jean-Michel</td>
+                       <td>🗑️</td>
+                   </tr>
+                   <tr class="row2">
+                       <td>F20190404-004</td>
+                       <td>04/04/2019</td>
+                       <td>Dundler Mifflin</td>
+                       <td>🗑️</td>
+                   </tr>
+                   <tr class="row1">
+                       <td>F20190404-003</td>
+                       <td>04/04/2019</td>
+                       <td>Pierre Cailloux</td>
+                       <td>🗑️</td>
+                   </tr>
+                   <tr class="row2">
+                       <td>F20190404-001</td>
+                       <td>04/04/2019</td>
+                       <td>Pied Pipper</td>
+                       <td>🗑️</td>
+                   </tr>
+                   <tr class="row1">
+                       <td>F20190403-654</td>
+                       <td>03/04/2019</td>
+                       <td>Raviga</td>
+                       <td>🗑️</td>
+                   </tr>
+               </tbody>
+            </table>
+
+
+            <table class="container1">
+      
+              <theader>
+                  <tr>
+                     <th>Last Companies</th>
+                  </tr>
+                  <tr>
+                     <th>Names</th>
+                     <th>TVA</th>
+                     <th>country</th>
+                     <th>Type</th>
+                     <th></th>
+                  </tr>
+              </theader>
+              <tbody>
+             <tr class="row1">
+                 <td>Raviga</td>
+                 <td>US456 654 342</td>
+                 <td>United States</td>
+                 <td>Fournisseur</td>
+                 <td>🗑️</td>
+             </tr>
+             <tr class="row2">
+                <td>Dunder Mifflin</td>
+                <td>US678 765 765</td>
+                <td>United States</td>
+                <td>Clients</td>
+                <td>🗑️</td>
+             </tr>
+             <tr class="row1">
+                 <td>Pierre Cailloux</td>
+                 <td>FR 678 908 654</td>
+                 <td>France</td>
+                 <td>Fournisseur</td>
+                 <td>🗑️</td>
+             </tr>
+             <tr class="row2">
+                <td>Belgalol</td>
+                <td>BE0876 654 665</td>
+                <td>Belgique</td>
+                <td>Fournisseur</td>
+                <td>🗑️</td>
+             </tr>
+             <tr class="row1">
+                 <td>Jouets Jean-Michel</td>
+                 <td>FR 677 544 000</td>
+                 <td>France</td>
+                 <td>Clients</td>
+                 <td>🗑️</td>
+             </tr>
+         </tbody>
+ </table>
+
+
+
+    <table class="container3">
+       
+          <theader>
+                <tr>
+                     <th class="titletab3" colspan="5">Last Contacts</th>
+                </tr>
+                 <tr>
+                     <th>Names</th>
+                     <th>phone</th>
+                     <th>E-mail</th>
+                     <th>compagny</th>
+                     <th></th>
+                 </tr>
+           </theader>
+           <tbody>
+                 <tr class="row1">
+                     <td>Peter Gregory</td>
+                     <td>555-4567</td>
+                     <td>peter.gregory@raviga.com</td>
+                     <td>Raviga</td>
+                     <td>🗑️</td>
+                 </tr>
+                 <tr class="row2">
+                     <td>Cameron Howe</td>
+                     <td>555-78967</td>
+                     <td>cam.howe@mutiny.net</td>
+                     <td>Mutiny</td>
+                     <td>🗑️</td>
+                 </tr>
+                 <tr class="row1">
+                     <td>Gavin Belson</td>
+                     <td>555-4213</td>
+                     <td>gavin@hooli.com</td>
+                     <td>Hoolir</td>
+                     <td>🗑️</td>
+                 </tr>
+                 <tr class=row2>
+                     <td>Jiang Yang</td>
+                     <td>555-4567</td>
+                     <td>jian.yan@phoque.off</td>
+                     <td>Phoque Off</td>
+                     <td>🗑️</td>
+                 </tr>
+                 <tr class="row1">
+                     <td>Bertram Gilfoyle</td>
+                     <td>555-098</td>
+                     <td>gilfoyle@piedpiper.com</td>
+                     <td>Pied Piper</td>
+                     <td>🗑️</td>
+                 </tr>
+           </tbody>
+    </table>
+ </div>  
 </main>
