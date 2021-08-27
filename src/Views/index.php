16c284eaf8fa54e3f82a46c0eaf499dd6b91b32d
juju2307 <metzjulie2@gmail.com>
2021-08-27 12:29:46 +0200

    indexvieuw css

--- src/Views/index.php
+++ src/Views/index.php
@@ -10,7 +10,7 @@
 
                 <theader>
                     <tr>
-                        <th>Last Invoices</th>
+                        <th class="titletab1">Last Invoices</th>
                     </tr>
                     <tr>
                         <th>Invoice Number</th>
@@ -59,7 +59,7 @@
       
               <theader>
                   <tr>
-                     <th>Last Companies</th>
+                     <th class="titletab2">Last Companies</th>
                   </tr>
                   <tr>
                      <th>Names</th>
@@ -70,88 +70,85 @@
                   </tr>
               </theader>
               <tbody>
-             <tr class="row1">
-                 <td>Raviga</td>
-                 <td>US456 654 342</td>
-                 <td>United States</td>
-                 <td>Fournisseur</td>
-                 <td>🗑️</td>
-             </tr>
-             <tr class="row2">
-                <td>Dunder Mifflin</td>
-                <td>US678 765 765</td>
-                <td>United States</td>
-                <td>Clients</td>
-                <td>🗑️</td>
-             </tr>
-             <tr class="row1">
-                 <td>Pierre Cailloux</td>
-                 <td>FR 678 908 654</td>
-                 <td>France</td>
-                 <td>Fournisseur</td>
-                 <td>🗑️</td>
-             </tr>
-             <tr class="row2">
-                <td>Belgalol</td>
-                <td>BE0876 654 665</td>
-                <td>Belgique</td>
-                <td>Fournisseur</td>
-                <td>🗑️</td>
-             </tr>
-             <tr class="row1">
-                 <td>Jouets Jean-Michel</td>
-                 <td>FR 677 544 000</td>
-                 <td>France</td>
-                 <td>Clients</td>
-                 <td>🗑️</td>
-             </tr>
-         </tbody>
- </table>
-
-
-
-    <table class="container3">
-       
-          <theader>
-                <tr>
-                     <th class="titletab3" colspan="5">Last Contacts</th>
-                </tr>
-                 <tr>
-                     <th>Names</th>
-                     <th>phone</th>
-                     <th>E-mail</th>
-                     <th>compagny</th>
-                     <th></th>
-                 </tr>
-           </theader>
-           <tbody>
-                 <tr class="row1">
-                     <td>Peter Gregory</td>
-                     <td>555-4567</td>
-                     <td>peter.gregory@raviga.com</td>
+                  <tr class="row1">
                      <td>Raviga</td>
+                     <td>US456 654 342</td>
+                     <td>United States</td>
+                     <td>Fournisseur</td>
                      <td>🗑️</td>
-                 </tr>
-                 <tr class="row2">
-                     <td>Cameron Howe</td>
-                     <td>555-78967</td>
-                     <td>cam.howe@mutiny.net</td>
-                     <td>Mutiny</td>
+                  </tr>
+                  <tr class="row2">
+                     <td>Dunder Mifflin</td>
+                     <td>US678 765 765</td>
+                     <td>United States</td>
+                     <td>Clients</td>
                      <td>🗑️</td>
-                 </tr>
-                 <tr class="row1">
-                     <td>Gavin Belson</td>
-                     <td>555-4213</td>
-                     <td>gavin@hooli.com</td>
-                     <td>Hoolir</td>
+                  </tr>
+                  <tr class="row1">
+                     <td>Pierre Cailloux</td>
+                     <td>FR 678 908 654</td>
+                     <td>France</td>
+                     <td>Fournisseur</td>
                      <td>🗑️</td>
-                 </tr>
-                 <tr class=row2>
-                     <td>Jiang Yang</td>
-                     <td>555-4567</td>
-                     <td>jian.yan@phoque.off</td>
-                     <td>Phoque Off</td>
+                  </tr>
+                  <tr class="row2">
+                     <td>Belgalol</td>
+                     <td>BE0876 654 665</td>
+                     <td>Belgique</td>
+                     <td>Fournisseur</td>
                      <td>🗑️</td>
+                  </tr>
+                  <tr class="row1">
+                     <td>Jouets Jean-Michel</td>
+                     <td>FR 677 544 000</td>
+                     <td>France</td>
+                     <td>Clients</td>
+                     <td>🗑️</td>
+                  </tr>
+             </tbody>
+          </table>
+          <table class="container1">
+       
+              <theader>
+                    <tr>
+                         <th class="titletab3">Last Contacts</th>
+                    </tr>
+                     <tr>
+                         <th>Names</th>
+                         <th>phone</th>
+                         <th>E-mail</th>
+                         <th>compagny</th>
+                         <th></th>
+                     </tr>
+               </theader>
+               <tbody>
+                    <tr class="row1">
+                         <td>Peter Gregory</td>
+                         <td>555-4567</td>
+                         <td>peter.gregory@raviga.com</td>
+                         <td>Raviga</td>
+                         <td>🗑️</td>
+                    </tr>
+                    <tr class="row2">
+                         <td>Cameron Howe</td>
+                         <td>555-78967</td>
+                         <td>cam.howe@mutiny.net</td>
+                         <td>Mutiny</td>
+                         <td>🗑️</td>
+                    </tr>
+                    <tr class="row1">
+                         <td>Gavin Belson</td>
+                         <td>555-4213</td>
+                         <td>gavin@hooli.com</td>
+                         <td>Hoolir</td>
+                         <td>🗑️</td>
+                    </tr>
+                    <tr class=row2>
+                         <td>Jiang Yang</td>
+                         <td>555-4567</td>
+                         <td>jian.yan@phoque.off</td>
+                         <td>Phoque Off</td>
+                         <td>🗑️</td>
                  </tr>
                  <tr class="row1">
                      <td>Bertram Gilfoyle</td>
