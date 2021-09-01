1574666c492b5cbe47ebc845e383d26350912d65
juju2307 <metzjulie2@gmail.com>
2021-09-01 15:07:30 +0200

    scss

--- src/Views/templates/footer.php
+++ src/Views/templates/footer.php
@@ -1,20 +1,24 @@
 <?php
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" media="all">
-    <link rel=stylesheet href="/assets/css/style.css">
-    <link rel=stylesheet href="/assets/css/reset.css">
-</head>
-<body>
     <footer>
         <div class="fixed-footer">
             <div class="footer">  © 2021 COGIP, Inc.     "Vive la COGIP!"</div>        
         </div>
     </footer>
+    <script>
+    var toggleButton = document.getElementById("toggleButton");
+    var navList = document.getElementById("navListId");
+    var admin = document.getElementById("admin");
+    var subMenuID = document.getElementById("subMenuID");
+
+    toggleButton.addEventListener('click', () => {
+        navList.classList.toggle('active');
+    });
+    
+
+    admin.addEventListener('click', () => {
+        subMenuID.style.display = "inline"; 
+    });
+</script>
 </body>
 </html>
