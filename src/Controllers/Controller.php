dc8f8250e95ba5da947f9c0bc95a546422a7d067
Adrien Lenoir <lenoiradrien1@gmail.com>
2021-09-01 12:00:28 +0200

    Patch user login/islogged & show dashboard in header if is logged & admin -> update -> company,contact,invoice

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -3,11 +3,37 @@
 namespace App\Controllers;
 
 
+use App\Helpers\RedirectHelper;
+
 abstract class Controller
 {
     public function render(string $view, array $params = [])
     {
         extract($params);
         require '../src/Views/' . $view . '.php';
+    }
+
+    public function userIsLogged() : bool
+    {
+        global $user;
+        return $user->isLoggedIn();
+    }
+
+    public function userHasPermission($permission) : bool
+    {
+        global $user;
+        return $user->hasPermission($permission);
+    }
+
+    public function userLogout()
+    {
+        global $user;
+        $user->logout();
+    }
+
+    public function redirect($location)
+    {
+        RedirectHelper::to($location);
+        exit();
     }
 }
