8ab2c7ad09231107bcf61c26c8e143f6ce86dd1b
Elsa Magalhaes <elsa.magalhaes@gmail.com>
2021-09-02 11:01:17 +0200

    db_update

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -19,7 +19,7 @@ class AuthController extends Controller
     public function login()
     {
         if ($this->userIsLogged()) {
-            $this->redirect('/');
+            $this->redirect('/admin');
             return;
         }
 
